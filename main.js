d419ae82063bad2ef351ef4dba4fa90f75d699d9
Szymon Rączka <116732045+screenfluent@users.noreply.github.com>
2023-10-10 17:59:22 +0200

    More learning js and other crazy things

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const firstName = 'Krzysztof';
 const age = 30;
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
+
+const header = document.querySelector('.page-header__heading--js');
+
+header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat........`;
