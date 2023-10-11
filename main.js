660cf139812fbe21afd1767814517f99506e3248
Szymon Rączka <116732045+screenfluent@users.noreply.github.com>
2023-10-11 11:29:24 +0200

    Continue learning

--- main.js
+++ main.js
@@ -1,8 +1,32 @@
-const firstName = 'Krzysztof';
-const age = 30;
+const firstNames = ['Adam', 'Bartosz', 'Cezary', 'Dariusz', 'Edward'];
+const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+const age = Math.floor(Math.random() * 50) + 18;
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat.`);
 
 const header = document.querySelector('.page-header__heading--js');
 
-header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat........`;
+header.innerHTML = `Nazywam się ${firstName} i mam ${age} lat.`;
+
+const calculate = myNumber => (myNumber+4)*7
+
+
+calculate(1)
+
+const result = calculate(2);
+console.log(result);
+
+const deathStar = {
+    diameter: 120000,
+    fire: (target) => {
+        console.log(`${target} destroyed 💥`)
+    },
+    isOperating: true,
+    levels: 357,
+    name: 'Death Star',
+    population: 10000
+}
+
+console.log(deathStar.name)
+
+deathStar.fire('Rebel ship')
